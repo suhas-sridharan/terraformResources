41cc99cdc6ece969743a67f3e0688c9216a4da35
Suhas Sridharan <73169664+suhas-sridharan@users.noreply.github.com>
2021-04-30 12:06:45 +0530

    Update config.tf

--- aws-module-1/config.tf
+++ aws-module-1/config.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket" "terraform_state_tathagat_test" {
   # Enable versioning so we can see the full revision history of our
   # state files
   versioning {
-    enabled = true
+    enabled = false
   }
   # Enable server-side encryption by default
   server_side_encryption_configuration {
