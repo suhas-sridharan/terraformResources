659f59e96441428da42d18038a0cdb7809417fd5
Suhas Sridharan <73169664+suhas-sridharan@users.noreply.github.com>
2021-05-03 23:43:31 +0530

    Update config.tf

--- aws-with-vars/config.tf
+++ aws-with-vars/config.tf
@@ -3,7 +3,7 @@ variable "access_key" {}
 variable "secret_key" {}
 
 
-variable "tag" {
+variable "tag_name" {
   default = "tathagat-test-1"
 }
 
@@ -11,18 +11,19 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   region = "us-east-1"
+  tag_name = var.tag_name
 }
 
 resource "aws_instance" "ec2_instance_test" {
   ami           = "ami-0663143d1f1caa3bf"
   instance_type = "t2.micro"
   tags = {
-    Name = var.tag
+    Name = var.tag_name
   }
 }
 
 output "tag" {
-  value = var.tag
+  value = var.tag_name
 }
 
 output "region" {
