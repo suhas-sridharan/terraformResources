a84d972bebc0b09ad8a2159e9a534922dc7b6375
Suhas Sridharan <73169664+suhas-sridharan@users.noreply.github.com>
2021-05-03 23:26:56 +0530

    Create config.tf

---
+++ aws-sg/config.tf
@@ -0,0 +1,32 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+    default = "us-east-1"
+}
+variable "tag_name" {}
+provider "aws" {
+       region = var.region
+       access_key = var.access_key
+       secret_key = var.secret_key
+}
+
+
+resource "aws_security_group" "sg-test" {
+  description = "dev_test security group allows ssh for everyone"
+  name        = "allow-ssh-${var.tag_name}"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+output "security_group" {
+    value = ["${aws_security_group.sg-test.id}"]
+}
+
+output "region" {
+    value = var.region
+}
